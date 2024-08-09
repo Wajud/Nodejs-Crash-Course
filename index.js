cbeda05377e0f18880d3f5624d555bd640507178
Wajud <wajudkareem757@gmail.com>
2024-08-09 12:22:15 +0100

    start of http module

---
+++ index.js
@@ -0,0 +1,10 @@
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+// console.log(generateRandomNumber());
+// console.log(`Celcius to Fahrenheit:  ${celciusToFahrenheit(0)}`);
+
+import { posts, getPostsLength } from "./postController.js";
+import getPosts from "./postController.js";
+
+// console.log(posts);
+console.log(getPostsLength());
